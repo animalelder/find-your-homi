373559e073fb58e961212ec05afcda2fc56fe3c0
animalelder <wllearned@icloud.com>
2025-02-07 14:16:14 -0800

    Added HOME link in the nav bar

--- components/nav-bar.jsx
+++ components/nav-bar.jsx
@@ -10,22 +10,26 @@ export function NavBar() {
         <nav className="group z-50 flex w-full max-w-6xl items-center justify-between rounded-full bg-white px-6 py-4 font-['Urbanist'] text-lg font-medium tracking-widest shadow-white hover:shadow-lg hover:outline hover:outline-primaryGreen/65">
           {/* Logo */}
           <div>
-            <Image
-              src="https://pqrdckeuqfydcgbgrxcg.supabase.co/storage/v1/object/public/assets//logo.png"
-              width={40}
-              height={40}
-              alt="Logo"
-              className="h-10"
-            />
+            <Link href="/">
+              <Image
+                src="https://pqrdckeuqfydcgbgrxcg.supabase.co/storage/v1/object/public/assets//logo.png"
+                width={40}
+                height={40}
+                alt="Logo"
+                className="h-10"
+              />
+            </Link>
           </div>
 
           {/* Navigation Links */}
           <ul className="hidden space-x-8 text-lg text-gray-700 sm:flex">
-            <li
-              className={`cursor-pointer rounded-xl px-4 py-1 hover:bg-primaryGreen hover:text-white`}
-            >
-              Home
-            </li>
+            <Link href="/">
+              <li
+                className={`cursor-pointer rounded-xl px-4 py-1 hover:bg-primaryGreen hover:text-white`}
+              >
+                Home
+              </li>
+            </Link>
             <Link href="/properties">
               <li
                 className={`cursor-pointer rounded-xl px-4 py-1 hover:bg-primaryGreen hover:text-white`}
